074636a24f46cbd94591bcea6f168bcb622afd1a
mo <mo@Django-VM>
2014-06-16 23:28:29 +0200

    first commit - main stuff initialized

---
+++ main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from main.models import Schrank
+from main.models import Kasten
+from main.models import Typ
+
+admin.site.register(Schrank)
+admin.site.register(Kasten)
+admin.site.register(Typ)
+# Register your models here.
---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Typ(models.Model):
+	NAME_CHOICES = (
+		('MA', 'Mate'),
+		('MC', 'Mate-Cola'),
+		('MG', 'Mate-Granat'),
+		('SO', 'sonstiges'),
+	)
+	name = models.CharField('Inhalt des Kastens', 
+									max_length=2,
+									choices=NAME_CHOICES,
+									default='SO',
+									primary_key=True)
+	hoehe = models.IntegerField('Hoehe des Kastens in Millimeter (mm)')
+
+	def __unicode__(self): 
+		return self.name
+
+class Kasten(models.Model):
+	ID = models.IntegerField('ID des Kastens', default=0, primary_key=True)
+	voll = models.BooleanField('Status des Kastens')
+	typ = models.ForeignKey(Typ)
+	
+	def __unicode__(self):
+		return u'%s %s' % (self.typ, self.voll)
+
+class Schrank(models.Model):
+	nummer = models.IntegerField('SchrankNummer', default=0)
+	raum = models.CharField('Raum wo Schrank steht', max_length=6)
+	kaesten = models.ForeignKey(Kasten)
+
+	def __unicode__(self): 
+		return u'%s %s' % (self.raum, self.nummer)
