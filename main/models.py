74dab42a511fa50174dfdef73ee17787ef9461c2
mo <mo@Django-VM>
2014-07-10 22:56:58 +0200

    restructured DB because Django foreign keys only work in one direction

--- main/models.py
+++ main/models.py
@@ -15,20 +15,21 @@ class Typ(models.Model):
 	hoehe = models.IntegerField('Hoehe des Kastens in Millimeter (mm)')
 
 	def __unicode__(self): 
-		return self.name
+		return self.get_name_display()
+
+class Schrank(models.Model):
+	nummer = models.CharField('eineindeutige SchrankNummer. Format E306.1', max_length=6, default=0, primary_key=True)
+	raum = models.CharField('Raum wo Schrank steht', max_length=6)
+	hoehe = models.IntegerField('Hoehe des Schrankes', default=0)
+
+	def __unicode__(self): 
+		return u'%s' % (self.nummer)
 
 class Kasten(models.Model):
 	ID = models.IntegerField('ID des Kastens', default=0, primary_key=True)
 	voll = models.BooleanField('Status des Kastens')
 	typ = models.ForeignKey(Typ)
+	schrank = models.ForeignKey(Schrank)
 	
 	def __unicode__(self):
-		return u'%s %s' % (self.typ, self.voll)
-
-class Schrank(models.Model):
-	nummer = models.CharField('eineindeutige SchrankNummer. Format E306.1', max_length=6, default=0, primary_key=True)
-	raum = models.CharField('Raum wo Schrank steht', max_length=6)
-	kaesten = models.ForeignKey(Kasten)
-
-	def __unicode__(self): 
-		return u'%s' % (self.nummer)
+		return u'%s %s %s' % (self.typ, self.schrank, self.voll)
