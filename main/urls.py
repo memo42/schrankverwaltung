085910e68810e3f12d257180a71c5a52721855e3
mo <mo@Django-VM>
2014-07-10 08:18:20 +0200

    adjusted urls and started to read DB values in views

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	# example: /main/	
+	url(r'^$', views.uebersicht, name='uebersicht'),
+	# example: /main/e306.1/
+	url(r'^(?P<schranknummer>E\d+.\d)/$', views.schrank, name='Schrankuebersicht'),
 )
--- main/views.py
+++ main/views.py
@@ -8,6 +8,10 @@ from main.models import Typ
 from main.models import Kasten
 from main.models import Schrank
 
-def index(request):
+def schrank(request, schranknummer):
+	schrank = Schrank.objects.get(nummer="E306.1")
 	#template = loader.get_template('main/index.html')
-	return render(request, 'main/new.html')
+	#return render(request, 'main/new.html')
+	return HttpResponse("%s" % schrank.kaesten.typ)
+def uebersicht(request):
+	return HttpResponse("Uebersicht") 
