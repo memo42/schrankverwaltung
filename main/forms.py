ade00e2ff5fc74dfef7f797282a3e6d0276cbe31
memo <mo@docker>
2014-08-26 18:26:05 +0000

    added validation of input < 10 to form

--- main/forms.py
+++ main/forms.py
@@ -7,3 +7,34 @@ class NameForm(forms.Form):
 	BI = forms.IntegerField(label='Bier', widget=forms.TextInput(attrs={'name': 'Bier', 'class': "form-control input-number", 'min': '0', 'max': '10'  }))
 	SO = forms.IntegerField(label='Sonstiges', widget=forms.TextInput(attrs={'name': 'Sonstiges', 'class': "form-control input-number", 'min': '0', 'max': '10'  }))
 	LG = forms.IntegerField(label='Leergut', widget=forms.TextInput(attrs={'name': 'Leergut', 'class': "form-control input-number", 'min': '0', 'max': '10'  }))
+
+	def clean_MA(self):
+		MA = self.cleaned_data['MA']
+		if MA > 10:
+			raise forms.ValidationError("Mate out of bounds exception")
+		return MA
+	def clean_MG(self):
+		MG = self.cleaned_data['MG']
+		if MG > 10:
+			raise forms.ValidationError("Mate out of bounds exception")
+		return MG
+	def clean_MC(self):
+		MC = self.cleaned_data['MC']
+		if MC > 10:
+			raise forms.ValidationError("Mate out of bounds exception")
+		return MC
+	def clean_BI(self):
+		BI = self.cleaned_data['BI']
+		if BI > 10:
+			raise forms.ValidationError("Mate out of bounds exception")
+		return BI
+	def clean_SO(self):
+		SO = self.cleaned_data['SO']
+		if SO > 10:
+			raise forms.ValidationError("Mate out of bounds exception")
+		return SO
+	def clean_LG(self):
+		LG = self.cleaned_data['LG']
+		if LG > 10:
+			raise forms.ValidationError("Mate out of bounds exception")
+		return LG
