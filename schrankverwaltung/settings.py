0909955c57c8310d9ed68d955adbd81fc6ea180d
memo <mo@miemo>
2014-09-30 18:37:04 +0200

    removed SECRET_KEY from settings.py file

--- schrankverwaltung/settings.py
+++ schrankverwaltung/settings.py
@@ -11,20 +11,24 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from ConfigParser import RawConfigParser
 
+config = RawConfigParser()
+config.read('schrankverwaltung/settings.ini')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5!l#b5(508uypp702*kxegbo8ei!-vxx#-y%@q&fn**&wx9*a0'
+SECRET_KEY = config.get('secrets','SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.miemo.de',
+						'www.miemo.de.',]
 
 
 # Application definition
@@ -81,3 +85,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = '/usr/local/share/schrank/main/static/'
