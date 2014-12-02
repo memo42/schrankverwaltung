63bf524d1cb9306751b2dec15fd5a91b98a19209
memo <mo@miemo.pc>
2014-12-02 21:47:25 +0100

    changed view not to create cupboards that don't exist but to return an error

--- main/views.py
+++ main/views.py
@@ -59,48 +59,50 @@ def raum_uebersicht(request, raumnummer):
 
 # Reads the number of crates in one part of the cupboard, displays them and lets the user change them.
 def schrank(request, schranknummer):
-	typen = Typ.objects.all()
-	schrank = Kasten.objects.filter(schrank=schranknummer)
-	kaesten = {}
-	initial = {}
-	for i in typen:
-		kaesten[i] = Kasten.objects.filter(schrank=schranknummer).filter(typ=i)
-		initial[str(i)] = len(kaesten[i])
-	
-	template = loader.get_template('main/schrank.html')
-	# if this is a POST request the form data is processed here
-	if request.method == 'POST':
-		# creates a form instance and populates it with data from the request
-		form = NameForm(request.POST)
-		success = ''
 
-		# checks whether it's valid
-		#print form.errors
-		if ( form.is_valid() ):
-			for kasten in typen:
-				# in case the number of crates of type kasten was reduced
-				if (len(kaesten[kasten]) > form.cleaned_data[str(kasten)]): 
-					success = 'Saved'
-					for i in range(0, len(kaesten[kasten]) - form.cleaned_data[str(kasten)]):
-						kaesten[kasten].first().delete()
-				# in case the number of crates of type kasten was increased
-				elif (len(kaesten[kasten]) < form.cleaned_data[str(kasten)]):
-					success = 'Saved'
-					for i in range(0,(form.cleaned_data[str(kasten)] - len(kaesten[kasten]))):
-						# if there is no matching "schrank" object a new one is created
-						if not Schrank.objects.filter(nummer=schranknummer):
-							c = Schrank(nummer=schranknummer, hoehe=100)
-							c.save()
-						b = Kasten(typ=kasten, schrank=Schrank.objects.filter(nummer=schranknummer)[0])
-						b.save()
-			
-			context = RequestContext(request, {'form': form, 'schranknummer': schranknummer, 'success': success})	
-			return HttpResponse(template.render(context))
-		# if the returned form data is not valid
+	# if there is no matching "schrank" object throw error 
+	if Schrank.objects.filter(nummer=schranknummer):
+		typen = Typ.objects.all()
+		schrank = Kasten.objects.filter(schrank=schranknummer)
+		kaesten = {}
+		initial = {}
+		for i in typen:
+			kaesten[i] = Kasten.objects.filter(schrank=schranknummer).filter(typ=i)
+			initial[str(i)] = len(kaesten[i])
+		
+		template = loader.get_template('main/schrank.html')
+		# if this is a POST request the form data is processed here
+		if request.method == 'POST':
+			# creates a form instance and populates it with data from the request
+			form = NameForm(request.POST)
+			success = ''
+	
+			# checks whether it's valid
+			#print form.errors
+			if ( form.is_valid() ):
+				for kasten in typen:
+					# in case the number of crates of type kasten was reduced
+					if (len(kaesten[kasten]) > form.cleaned_data[str(kasten)]): 
+						success = 'Saved'
+						for i in range(0, len(kaesten[kasten]) - form.cleaned_data[str(kasten)]):
+							kaesten[kasten].first().delete()
+					# in case the number of crates of type kasten was increased
+					elif (len(kaesten[kasten]) < form.cleaned_data[str(kasten)]):
+						success = 'Saved'
+						for i in range(0,(form.cleaned_data[str(kasten)] - len(kaesten[kasten]))):
+							b = Kasten(typ=kasten, schrank=Schrank.objects.filter(nummer=schranknummer)[0])
+							b.save()
+				
+					context = RequestContext(request, {'form': form, 'schranknummer': schranknummer, 'success': success})	
+					return HttpResponse(template.render(context))
+			# if the returned form data is not valid
+			else:
+				return HttpResponse('Error: you somehow managed to enter invalid data')
+		# if it is a GET request a blank form is created
 		else:
-			return HttpResponse('Error: you somehow managed to enter invalid data')
-	# if it is a GET request a blank form is created
+			form = NameForm(initial)
+			context = RequestContext(request, {'form': form, 'schranknummer': schranknummer,})	
+			return HttpResponse(template.render(context))
+	
 	else:
-		form = NameForm(initial)
-		context = RequestContext(request, {'form': form, 'schranknummer': schranknummer,})	
-		return HttpResponse(template.render(context))
+		return HttpResponse('Error: not a valid cupboard')
