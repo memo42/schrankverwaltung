1338d985a76b055727d5c02fde61938bfcc79b5e
memo@mittelmeer <memo_23@gmx.de>
2016-04-19 08:51:51 +0200

    added links to type and room labels

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 from main.models import Typ
 from main.models import Kasten
@@ -7,13 +8,13 @@ from main.models import Schrank
 maxAnzahl = 100
 class NameForm(forms.Form):
 	mylabel = {}
-	mylabel['Mate'] = 'Mate'
-	mylabel['Mate-Granat'] = 'Mate-Granat'
-	mylabel['Mate-Cola'] = 'Mate-Cola'
-	mylabel['Mate-Eistee'] = 'Mate-Eistee'
-	mylabel['Bier'] = 'Bier'
-	mylabel['Sonstiges'] = 'Sonstiges'
-	mylabel['Leergut'] = 'Leergut'
+	mylabel['Mate'] = mark_safe('<a href="../MA">Mate</a>')
+	mylabel['Mate-Granat'] = mark_safe('<a href="../MG">Mate-Granat</a>')
+	mylabel['Mate-Cola'] = mark_safe('<a href="../MC">Mate-Cola</a>')
+	mylabel['Mate-Eistee'] = mark_safe('<a href="../ME">Mate-Eistee</a>')
+	mylabel['Bier'] = mark_safe('<a href="../BI">Bier</a>')
+	mylabel['Sonstiges'] = mark_safe('<a href="../SO">Sonstiges</a>')
+	mylabel['Leergut'] = mark_safe('<a href="../LG">Leergut</a>')
 
 	MA = forms.IntegerField(label=mylabel['Mate'], widget=forms.TextInput(attrs={'name': 'MA', 'class': "form-control input-number", 'min': '0', 'max': maxAnzahl }))
 	MG = forms.IntegerField(label=mylabel['Mate-Granat'], widget=forms.TextInput(attrs={'name': 'MG', 'class': "form-control input-number", 'min': '0', 'max': maxAnzahl  }))
@@ -60,17 +61,23 @@ class NameForm(forms.Form):
 		return LG
 
 class TypeForm(forms.Form):
+	L1 = 'E201'
+	L2 = 'E302'
+	L3 = 'E303'
+	L4 = 'E304'
+	L5 = 'E306'
+	L6 = 'E013'
 	mylabel = {}
-	mylabel['E201'] = 'E201'
-	mylabel['E302'] = 'E302'
-	mylabel['E303'] = 'E303'
-	mylabel['E304'] = 'E304'
-	mylabel['E306'] = 'E306'
-	mylabel['E013'] = 'E013'
+	mylabel[L1] = mark_safe('<a href="../' + L1 + '">' + L1 + '</a>')
+	mylabel[L2] = mark_safe('<a href="../' + L2 + '">' + L2 + '</a>')
+	mylabel[L3] = mark_safe('<a href="../' + L3 + '">' + L3 + '</a>')
+	mylabel[L4] = mark_safe('<a href="../' + L4 + '">' + L4 + '</a>')
+	mylabel[L5] = mark_safe('<a href="../' + L5 + '">' + L5 + '</a>')
+	mylabel[L6] = mark_safe('<a href="../' + L6 + '">' + L6 + '</a>')
 
-	E201 = forms.IntegerField(label=mylabel['E201'], widget=forms.TextInput(attrs={'name': 'E201', 'class': "form-control input-number"}))
-	E302 = forms.IntegerField(label=mylabel['E302'], widget=forms.TextInput(attrs={'name': 'E302', 'class': "form-control input-number"}))
-	E303 = forms.IntegerField(label=mylabel['E303'], widget=forms.TextInput(attrs={'name': 'E303', 'class': "form-control input-number"}))
-	E304 = forms.IntegerField(label=mylabel['E304'], widget=forms.TextInput(attrs={'name': 'E304', 'class': "form-control input-number"}))
-	E306 = forms.IntegerField(label=mylabel['E306'], widget=forms.TextInput(attrs={'name': 'E306', 'class': "form-control input-number"}))
-	E013 = forms.IntegerField(label=mylabel['E013'], widget=forms.TextInput(attrs={'name': 'E013', 'class': "form-control input-number"}))
+	E201 = forms.IntegerField(label=mylabel[L1], widget=forms.TextInput(attrs={'name': L1, 'class': "form-control input-number"}))
+	E302 = forms.IntegerField(label=mylabel[L2], widget=forms.TextInput(attrs={'name': L2, 'class': "form-control input-number"}))
+	E303 = forms.IntegerField(label=mylabel[L3], widget=forms.TextInput(attrs={'name': L3, 'class': "form-control input-number"}))
+	E304 = forms.IntegerField(label=mylabel[L4], widget=forms.TextInput(attrs={'name': L4, 'class': "form-control input-number"}))
+	E306 = forms.IntegerField(label=mylabel[L5], widget=forms.TextInput(attrs={'name': L5, 'class': "form-control input-number"}))
+	E013 = forms.IntegerField(label=mylabel[L6], widget=forms.TextInput(attrs={'name': L6, 'class': "form-control input-number"}))
