1d7312779f22ec1329cb5fcc58814aefa826bd2b
memo@mittelmeer <memo_23@gmx.de>
2016-04-20 16:24:06 +0200

    added long name for types to database

--- main/models.py
+++ main/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Typ(models.Model):
-	name = models.CharField('Inhalt des Kastens',
-									max_length=20,
+	name = models.CharField('Inhalt des Kastens (Abkuerzung)',
+									max_length=2,
 									primary_key=True)
+	longname = models.CharField('Inhalt des Kastens (lang)', max_length=20)
 	hoehe = models.IntegerField('Hoehe des Kastens in Millimeter (mm)')
 
 	def __unicode__(self):
-		return u'%s' % (self.name)
+		return u'%s' % (self.longname)
 
 class Schrank(models.Model):
 	nummer = models.CharField('eineindeutige SchrankNummer. Format E306.1', max_length=6, default=0, primary_key=True)
@@ -23,4 +24,3 @@ class Kasten(models.Model):
 
 	def __unicode__(self):
 		return u'%s %s' % (self.typ, self.schrank)
-
